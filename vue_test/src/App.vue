fda68def2313d84eec67357cb1243213426019d5
yh <981976482@qq.com>
2023-02-23 00:05:25 +0800

    addddd

---
+++ vue_test/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <img src="./assets/logo.png" alt="logo">
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+//引入组件
+import School from './components/School'
+import Student from './components/Student'
+
+// 暴露组件
+export default {
+  name: 'App',
+  //注册组件
+  components: {
+    School,
+    Student
+  }
+}
+</script>
+
+<!--<style>-->
+<!--#app {-->
+<!--  font-family: Avenir, Helvetica, Arial, sans-serif;-->
+<!--  -webkit-font-smoothing: antialiased;-->
+<!--  -moz-osx-font-smoothing: grayscale;-->
+<!--  text-align: center;-->
+<!--  color: #2c3e50;-->
+<!--  margin-top: 60px;-->
+<!--}-->
+<!--</style>-->
---
+++ vue_test/src/main.js
@@ -0,0 +1,21 @@
+/*
+	该文件是整个项目的入口文件, 执行npm run serve 的时候，就是执行 src/main.js
+*/
+//引入Vue
+import Vue from 'vue'
+//引入App组件，它是所有组件的父组件
+import App from './App.vue'
+//关闭vue的生产提示
+Vue.config.productionTip = false
+
+//创建Vue实例对象---vm
+new Vue({
+    //render函数完成了这个功能：将App组件放入容器中
+  render: h => h(App),
+    // render:q=> q('h1','你好啊')
+
+    // template:`<h1>你好啊</h1>`,
+    // components:{App},
+
+    // 把vm挂载到id=app这个容器，在public/index.html中, 所以渲染的时候其实就是渲染public/index.html
+}).$mount('#app')
