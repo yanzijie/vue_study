85c9307ebb5e6da7a87aa481805e023876bcfab3
zijian <>
2023-02-24 17:16:35 +0800

    add

---
+++ vue_test/06_src_scoped样式/components/School.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="demo">
+		<h2 class="title">学校名称：{{name}}</h2>
+		<h2>学校地址：{{address}}</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'School',
+		data() {
+			return {
+				name:'尚硅谷atguigu',
+				address:'北京',
+			}
+		}
+	}
+</script>
+
+<style scoped>
+  /*scoped样式作用:只作用于当前的界面,让样式在局部生效，防止冲突*/
+	.demo{
+		background-color: skyblue;
+	}
+</style>
---
+++ vue_test/06_src_scoped样式/components/Student.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="demo">
+		<h2 class="title">学生姓名：{{name}}</h2>
+		<h2 class="atguigu">学生性别：{{sex}}</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Student',
+		data() {
+			return {
+				name:'张三',
+				sex:'男'
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+  //lang=“less” 可以敲层级关系的样式
+  /*scoped样式作用:只作用于当前的界面,让样式在局部生效，防止冲突*/
+	.demo{
+		background-color: pink;
+		.atguigu{
+			font-size: 40px;
+		}
+	}
+</style>
